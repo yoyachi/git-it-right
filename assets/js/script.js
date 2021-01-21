f9bd20450eb7e0bd25b05088b03f7d76d52188fa
Raymond Lopez <raymondlopez13@yahoo.com>
2021-01-20 21:33:01 -0600

    Fixed bug that would fetch multiple objects instead of one

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,33 +6,39 @@ var getRandomNumber = function() {
     var number = Math.floor(Math.random() * Math.floor(movieNamesObj.length));
     return number;
 }
-
-// Establish api key and fetch actors of movie from API
-var apikey = 'http://www.omdbapi.com/?i=tt3896198&apikey=da94de50';
-var getMovieData = function() {
-    var rando = getRandomNumber();
-    checkMovie(rando);
-    console.log(rando);
+// Fetch
+var fetchFun = function(rando) {
     fetch(apikey+'&t='+movieNamesObj[rando]).then(function(response) {
         response.json().then(function(data) {
             console.log(data);
         })
     })
 };
+// Establish api key and fetch actors of movie from API
+var apikey = 'http://www.omdbapi.com/?i=tt3896198&apikey=da94de50';
+var getMovieData = function() {
+    var rando = getRandomNumber();
+    checkMovie(rando);
+    
+};
 
 // check if the random number that has been generated, has already been used
-var  checkMovie = function(rando) {
+var checkMovie = function(rando) {
+    console.log(rando);
     var randomNumbersUsed = localStorage.getItem('randomNumbers');
     if(randomNumbersUsed == null) {
         var arr = [];
         arr.push(rando);
         localStorage.setItem('randomNumbers', JSON.stringify(arr));
+        fetchFun(rando);
         return;
     }
     var arr = []; 
     arr = JSON.parse(randomNumbersUsed);
     if (arr.length === movieNamesObj.length) {
         localStorage.removeItem('randomNumbers');
+        getMovieData();
+        return;
     }
     for (i = 0; i < arr.length; i++) {
         if (rando === arr[i]) {
@@ -42,5 +48,6 @@ var  checkMovie = function(rando) {
     }
     arr.push(rando);
     localStorage.setItem('randomNumbers', JSON.stringify(arr));
+    fetchFun(rando);
 }
 getMovieData();
