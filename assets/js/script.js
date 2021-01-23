31f064d1e4a1b3dab6dd33591c0b77b58f3d3fe5
Raymond Lopez <raymondlopez13@yahoo.com>
2021-01-22 23:40:03 -0600

    Basic quiz funcitonality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,20 @@
 // Movie names
 var movieNamesObj = ['Django Unchained', 'Once upon a time... in Hollywood', 'Black Panther', 'Maze Runner', 'Deadpool', 'Crazy Rich Asians', 'A Star is Born', 'Bohemian Rhapsody', 
 'Jaws', 'Rocky', 'Back to the Future'];
+
+
+var wrongActors = ["Tom Hanks", "Will Smith", "Leonardo DiCaprio", "Morgan Freeman", "Brad Pitt", "Denzel Washington", 
+"Johnny Depp", "Harrison Ford", "Tom Cruise", "Al Pacino", "Matt Damon", "Keanu Reeves", "Jack Nicholson", "Meryl Streep", 
+"Natalie Portman", "Charlize Theron", "Emma Stone", "Viola Davis", "Brie Larson", "Octavia Spencer", "Amy Adams", "Drew Barrymore", 
+"Jennifer Garner", "Sigourney Weaver", "Audrey Hepburn", "Betty Davis"];
+
+var score = localStorage.getItem('score');
+if (score == null) {
+    score = 0;
+    localStorage.setItem('score', score);
+}
+
+
 // Get a random number to send into getMovieData
 var getRandomNumber = function() {
     var number = Math.floor(Math.random() * Math.floor(movieNamesObj.length));
@@ -10,7 +24,7 @@ var getRandomNumber = function() {
 var fetchFun = function(rando) {
     fetch(apikey+'&t='+movieNamesObj[rando]).then(function(response) {
         response.json().then(function(data) {
-            console.log(data);
+            appendItems(data);
         })
     })
 };
@@ -59,5 +73,67 @@ var checkMovie = function(rando) {
     arr.push(rando);
     localStorage.setItem('randomNumbers', JSON.stringify(arr));
     fetchFun(rando);
-}
+};
+
+
+// append data to page
+var appendItems = function(data) {
+    // set movie title
+    var movieName = data.Title;
+    document.getElementById('movieName').textContent = movieName;
+    // get actors
+    var actors = data.Actors;
+    // seperate string into an arr 
+    actors = actors.split(', ');
+    for (i=0; i<actors.length; i++) {
+        var listEl = document.createElement('li');
+        listEl.setAttribute('class', 'pure-menu-item pure-menu-link');
+        listEl.setAttribute('id', 'correct');
+        listEl.textContent = actors[i];
+        var answerUl = document.querySelector('#answerChoices');
+        answerUl.appendChild(listEl);
+    }
+    var listEl = document.createElement('li');
+    listEl.setAttribute('class', 'pure-menu-item pure-menu-link');
+    listEl.setAttribute('id', 'incorrect');
+    listEl.textContent = wrongActors[0];
+    var answerUl = document.querySelector('#answerChoices');
+    answerUl.appendChild(listEl);
+    getRandomNumber();
+};
+
+
+// click event
+var answerChoices = document.querySelector('#answerChoices');
+answerChoices.addEventListener('click', function() {
+    var answerChoice = event.target;
+    if (answerChoice.id === 'correct') {
+        answerChoice.setAttribute('class', 'bg-green pure-menu-item pure-menu-link');
+        score++;
+        var localScore = localStorage.getItem('score');
+        if (localScore == null) {
+            localStorage.setItem('score', score);
+            return;
+        }
+        answerChoice.setAttribute('id', '');
+        console.log(answerChoice);
+    } else if (answerChoice.id === 'incorrect') {
+        answerChoice.setAttribute('class', 'bg-red pure-menu-item pure-menu-link');
+        score--;
+        var localScore = localStorage.getItem('score');
+        if (localScore == null) {
+            localStorage.setItem('score', score);
+            return;
+        }
+        window.location.reload();
+    }
+    localStorage.setItem('score', score);
+    console.log(score);
+});
+
+
+
+
 getMovieData();
+
+
