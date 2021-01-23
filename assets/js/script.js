caf491316a0dff6f01637df089d6d20b76473952
Raymond Lopez <raymondlopez13@yahoo.com>
2021-01-22 21:33:21 -0600

    Added comments so code is easier to read

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ var getRandomNumber = function() {
     var number = Math.floor(Math.random() * Math.floor(movieNamesObj.length));
     return number;
 }
-// Fetch
+// Fetch function
 var fetchFun = function(rando) {
     fetch(apikey+'&t='+movieNamesObj[rando]).then(function(response) {
         response.json().then(function(data) {
@@ -14,8 +14,9 @@ var fetchFun = function(rando) {
         })
     })
 };
-// Establish api key and fetch actors of movie from API
+// Establish api key
 var apikey = 'http://www.omdbapi.com/?i=tt3896198&apikey=da94de50';
+//
 var getMovieData = function() {
     var rando = getRandomNumber();
     checkMovie(rando);
@@ -24,28 +25,37 @@ var getMovieData = function() {
 
 // check if the random number that has been generated, has already been used
 var checkMovie = function(rando) {
-    console.log(rando);
+    // Get numbers that have been used before
     var randomNumbersUsed = localStorage.getItem('randomNumbers');
+    // Check if the arr in local storage has any values in it
     if(randomNumbersUsed == null) {
         var arr = [];
         arr.push(rando);
         localStorage.setItem('randomNumbers', JSON.stringify(arr));
+        // Make the fetch call
         fetchFun(rando);
         return;
     }
-    var arr = []; 
+    // create empty arr if there are values in local storage
+    var arr = [];
+    // go through each item in local storage and push it to existing arr
     arr = JSON.parse(randomNumbersUsed);
+    // Check if all movies have already been used
     if (arr.length === movieNamesObj.length) {
         localStorage.removeItem('randomNumbers');
         getMovieData();
         return;
     }
+    // check if the random number has already been used
     for (i = 0; i < arr.length; i++) {
         if (rando === arr[i]) {
             getMovieData();
             return;
         }
     }
+    // if number hasn't been used, all numbers have been used, and if the local
+    // storage already has values in it, add new number to arr and fetch the movie
+    // associated with that value
     arr.push(rando);
     localStorage.setItem('randomNumbers', JSON.stringify(arr));
     fetchFun(rando);
