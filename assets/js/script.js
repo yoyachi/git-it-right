61edb85fe3d02b2e22496ec6539dcf62f25c37d9
Cody Kimb <codykimbell@gmail.com>
2021-01-23 19:17:13 -0600

    gif response for correct answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -120,6 +120,9 @@ answerChoices.addEventListener('click', function() {
         }
         answerChoice.setAttribute('id', '');
         console.log(answerChoice);
+
+        displayGif();
+
     } else if (answerChoice.id === 'incorrect') {
         answerChoice.setAttribute('class', 'bg-red pure-menu-item pure-menu-link');
         score--;
@@ -128,13 +131,45 @@ answerChoices.addEventListener('click', function() {
             localStorage.setItem('score', score);
             return;
         }
+
+        // page reload
         window.location.reload();
     }
     localStorage.setItem('score', score);
     console.log(score);
 });
 
+var displayGif = function() {
 
+
+  // Create a variable that will use `document.querySelector()` to target the `id` of the input 
+  // Use `.value` to capture the value of the input and store it in the variable
+  var searchTermMovie = document.getElementById("movieName").textContent;
+  var searchTermActor = event.target.textContent;
+
+  // Make a `fetch` request concatenating that variable to the query URL
+  var apiUrl = "https://api.giphy.com/v1/gifs/search?q=" + searchTermMovie + " " + searchTermActor + "&api_key=HvaacROi9w5oQCDYHSIk42eiDSIXH3FN"
+
+  fetch(apiUrl)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(response) {
+      console.log(response.data[0]);
+      
+      var gifDisplay = document.querySelector("#gif")
+      
+      // Empty out the <div> before we append a GIF to it
+      gifDisplay.innerHTML = ""
+      
+      var gifImg = document.createElement('img');
+      gifImg.setAttribute('src', response.data[0].images.fixed_height.url);
+     
+      // Append 'gifImg' to the <div>
+      gifDisplay.appendChild(gifImg)     
+  }) 
+
+}
 
 
 getMovieData();
